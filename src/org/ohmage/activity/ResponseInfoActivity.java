b35fcfaeada0ccbd75eecb1412de65698cf2ef18
Cameron Ketcham <cketcham@gmail.com>
2011-09-15 18:02:01 -0700

    updated the responseInfo activity and changed the ResponseInfoActivity to use the BaseInfoActivity class

--- src/org/ohmage/activity/ResponseInfoActivity.java
+++ src/org/ohmage/activity/ResponseInfoActivity.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2011 The Regents of the University of California
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,74 +15,63 @@
  ******************************************************************************/
 package org.ohmage.activity;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.ohmage.CampaignXmlHelper;
-import org.ohmage.PromptXmlParser;
 import org.ohmage.R;
 import org.ohmage.db.DbContract;
-import org.ohmage.db.DbHelper.Tables;
-import org.ohmage.prompt.AbstractPrompt;
-import org.ohmage.prompt.Prompt;
-import org.xmlpull.v1.XmlPullParserException;
+import org.ohmage.db.DbContract.Campaign;
+import org.ohmage.db.DbContract.PromptResponse;
+import org.ohmage.db.DbContract.Response;
+import org.ohmage.db.DbContract.Survey;
+import org.ohmage.db.DbContract.SurveyPrompt;
+import org.ohmage.prompt.photo.PhotoPrompt;
 
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources.NotFoundException;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.ListFragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
-import android.util.Log;
-import android.view.LayoutInflater;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter.ViewBinder;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * This Activity is used to display Information for an individual response. It
  * is called with {@link Intent#ACTION_VIEW} on the URI specified by
  * {@link DbContract.Response#getResponseUri(long)}
- * 
+ *
  * @author cketcham
- * 
+ *
  */
-public class ResponseInfoActivity extends FragmentActivity implements
-		LoaderManager.LoaderCallbacks<Cursor> {
-
-	private TextView mSurveyName;
-	private TextView mCampaignName;
-	private TextView mCompletedDate;
-	private View mEntityHeader;
+public class ResponseInfoActivity extends BaseInfoActivity implements
+LoaderManager.LoaderCallbacks<Cursor> {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.response_info_layout);
+		getActionBar().setTitle("Response");
 
 		FragmentManager fm = getSupportFragmentManager();
 
 		// Create the list fragment and add it as our sole content.
 		if (fm.findFragmentById(R.id.root_container) == null) {
 
-			ResponseInfoListFragment list = new ResponseInfoListFragment();
+			ResponsePromptsFragment list = new ResponsePromptsFragment();
 
-			fm.beginTransaction().add(R.id.root_container, list, "list")
-					.commit();
+			fm.beginTransaction().add(R.id.root_container, list, "list").commit();
 		}
 		// Prepare the loader. Either re-connect with an existing one,
 		// or start a new one.
@@ -93,22 +82,17 @@ public class ResponseInfoActivity extends FragmentActivity implements
 	public void onContentChanged() {
 		super.onContentChanged();
 
-		mEntityHeader = findViewById(R.id.entity_header_content);
 		mEntityHeader.setVisibility(View.GONE);
-		mSurveyName = (TextView) findViewById(R.id.entity_header);
-		mCampaignName = (TextView) findViewById(R.id.entity_header_sub1);
-		mCompletedDate = (TextView) findViewById(R.id.entity_header_sub2);
 	}
 
 	private interface ResponseQuery {
-		String[] PROJECTION = { Tables.RESPONSES + "." + DbContract.Response.CAMPAIGN_URN,
-				DbContract.Response.RESPONSE, Tables.RESPONSES + "." + DbContract.Response.SURVEY_ID,
-				DbContract.Response.TIME };
+		String[] PROJECTION = { Campaign.NAME,
+				Survey.TITLE,
+				Response.TIME };
 
-		int CAMPAIGN_URN = 0;
-		int RESPONSE = 1;
-		int SURVEY_ID = 2;
-		int TIME = 3;
+		int CAMPAIGN_NAME = 0;
+		int SURVEY_TITLE = 1;
+		int TIME = 2;
 	}
 
 	@Override
@@ -123,220 +107,182 @@ public class ResponseInfoActivity extends FragmentActivity implements
 			return;
 		}
 
-		final String surveyName = data.getString(ResponseQuery.SURVEY_ID);
-		final String completedDate = data.getString(ResponseQuery.TIME);
+		final String surveyName = data.getString(ResponseQuery.SURVEY_TITLE);
+		final Long completedDate = data.getLong(ResponseQuery.TIME);
 
-		try {
-			Map<String, String> campaignInfo = PromptXmlParser
-					.parseCampaignInfo(CampaignXmlHelper.loadCampaignXmlFromDb(
-							this, data.getString(ResponseQuery.CAMPAIGN_URN)));
+		mHeadertext.setText(surveyName);
+		mSubtext.setText(data.getString(ResponseQuery.CAMPAIGN_NAME));
+		SimpleDateFormat df = new SimpleDateFormat();
+		mNotetext.setText(df.format(new Date(completedDate)));
 
-			mSurveyName.setText(surveyName);
-			mCampaignName.setText(campaignInfo.get("campaign_name"));
-			SimpleDateFormat df = new SimpleDateFormat();
-			mCompletedDate.setText(df.format(new Date((Long
-					.valueOf(completedDate)))));
-
-			mEntityHeader.setVisibility(View.VISIBLE);
-		} catch (XmlPullParserException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		FragmentManager fm = getSupportFragmentManager();
-		ResponseInfoListFragment list = (ResponseInfoListFragment) fm
-				.findFragmentByTag("list");
-		if (list != null)
-			list.onLoadFinished(loader, data);
+		mEntityHeader.setVisibility(View.VISIBLE);
 	}
 
 	@Override
 	public void onLoaderReset(Loader<Cursor> loader) {
 		mEntityHeader.setVisibility(View.GONE);
-
-		FragmentManager fm = getSupportFragmentManager();
-		ResponseInfoListFragment list = (ResponseInfoListFragment) fm
-				.findFragmentByTag("list");
-		if (list != null)
-			list.onLoaderReset(loader);
 	}
 
-	public static class ResponseInfoListFragment extends ListFragment implements
-			LoaderManager.LoaderCallbacks<Cursor> {
+	public static class ResponsePromptsFragment extends ListFragment implements
+	LoaderManager.LoaderCallbacks<Cursor> {
 
 		private static final String TAG = "ResponseInfoListFragment";
 
 		// This is the Adapter being used to display the list's data.
-		JSONArrayAdapter mAdapter;
+		PromptResponsesAdapter mAdapter;
 
 		@Override
 		public void onActivityCreated(Bundle savedInstanceState) {
 			super.onActivityCreated(savedInstanceState);
 
-			Log.d("lm", "LoaderManager=" + getLoaderManager());
-
 			// We have no menu items to show in action bar.
 			setHasOptionsMenu(false);
 
+			getListView().setDivider(null);
+
 			// Create an empty adapter we will use to display the loaded data.
-			mAdapter = new JSONArrayAdapter(getActivity(),
-					android.R.layout.simple_list_item_2, new String[] {
-							"label", "display_value" }, new int[] {
-							android.R.id.text1, android.R.id.text2 });
+			mAdapter = new PromptResponsesAdapter(getActivity(), null,  new String[] {
+				SurveyPrompt.PROMPT_TEXT, PromptResponse.PROMPT_VALUE }, new int[] {
+				android.R.id.text1, R.id.prompt_value }, 0);
+
 			setListAdapter(mAdapter);
 
 			// Start out with a progress indicator.
 			setListShown(false);
-		}
 
-		@Override
-		public void onListItemClick(ListView l, View v, int position, long id) {
-			// Insert desired behavior here.
-			Log.i("FragmentComplexList", "Item clicked: " + id);
+			getLoaderManager().initLoader(0, null, this);
 		}
 
 		@Override
 		public Loader<Cursor> onCreateLoader(int id, Bundle bundle) {
-			return null;
+			return new CursorLoader(getActivity(),
+					DbContract.PromptResponse
+					.getPromptsByResponseID(ContentUris
+							.parseId(getActivity().getIntent()
+									.getData())),
+									null, DbContract.PromptResponse.PROMPT_VALUE + " !=?", new String[] { "NOT_DISPLAYED" }, null);
 		}
 
 		@Override
 		public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-			if (!data.moveToFirst()) {
-				return;
-			}
+			mAdapter.swapCursor(data);
 
-			List<Prompt> prompts = null;
-			try {
-				prompts = PromptXmlParser.parsePrompts(CampaignXmlHelper
-						.loadCampaignXmlFromDb(getActivity()
-								.getApplicationContext(), data
-								.getString(ResponseQuery.CAMPAIGN_URN)), data
-						.getString(ResponseQuery.SURVEY_ID));
-			} catch (NotFoundException e) {
-				Log.e(TAG, "Error parsing prompts from xml", e);
-			} catch (XmlPullParserException e) {
-				Log.e(TAG, "Error parsing prompts from xml", e);
-			} catch (IOException e) {
-				Log.e(TAG, "Error parsing prompts from xml", e);
-			}
-
-			// Swap the new data in
-			try {
-				final JSONArray jsonData = new JSONArray(
-						data.getString(ResponseQuery.RESPONSE));
-				for (int i = 0; i < jsonData.length(); i++) {
-					JSONObject jsonObject = jsonData.getJSONObject(i);
-					String prompt_id = jsonObject.getString("prompt_id");
-					for (Prompt prompt : prompts) {
-						if (((AbstractPrompt) prompt).getId().equals(prompt_id)) {
-							jsonObject
-									.put("label", ((AbstractPrompt) prompt)
-											.getDisplayLabel());
-							jsonObject.put("display_value", AbstractPrompt
-									.getDisplayValue((AbstractPrompt) prompt,
-											jsonObject.getString("value")));
-							break;
-						}
-					}
-				}
-				mAdapter.setData(jsonData);
-
-				// The list should now be shown.
-				if (isResumed()) {
-					setListShown(true);
-				} else {
-					setListShownNoAnimation(true);
-				}
-			} catch (JSONException e) {
-				// Unable to parse data
-				Log.e(TAG, "Unable to parse response json");
+			// The list should now be shown.
+			if (isResumed()) {
+				setListShown(true);
+			} else {
+				setListShownNoAnimation(true);
 			}
 		}
 
 		@Override
 		public void onLoaderReset(Loader<Cursor> loader) {
-			mAdapter.setData(null);
+			mAdapter.swapCursor(null);
 		}
 
-		/**
-		 * JSONArrayAdapter that renders the response JSON for a
-		 * {@link ResponseQuery}.
-		 */
-		private class JSONArrayAdapter extends BaseAdapter {
-			private JSONArray mData;
-			private final Context mContext;
-			private final int mLayout;
-			private final String[] mFrom;
-			private final int[] mTo;
+		private static class PromptResponsesAdapter extends SimpleCursorAdapter implements ViewBinder {
 
-			public JSONArrayAdapter(Context context, int layout, String[] from,
-					int[] to) {
-				super();
-				mContext = context;
-				mLayout = layout;
-				mFrom = from;
-				mTo = to;
+			public static final int TEXT_RESPONSE = 0;
+			public static final int IMAGE_RESPONSE = 1;
+
+			public PromptResponsesAdapter(Context context, Cursor c, String[] from,
+					int[] to, int flags) {
+				super(context, R.layout.response_prompt_list_item, c, from, to, flags);
+				setViewBinder(this);
 			}
 
 			@Override
-			public int getCount() {
-				return (mData == null) ? 0 : mData.length();
-			}
-
-			@Override
-			public JSONObject getItem(int position) {
-				try {
-					return (mData == null) ? null : mData
-							.getJSONObject(position);
-				} catch (JSONException e) {
-					return null;
-				}
-			}
-
-			@Override
-			public long getItemId(int position) {
-				// TODO Auto-generated method stub
+			public int getItemViewType(int position) {
+				if(getCursor().moveToPosition(position))
+					return getItemViewType(getCursor());
 				return 0;
 			}
 
-			@Override
-			public View getView(int position, View convertView, ViewGroup parent) {
-				TextView[] views;
-				if (convertView == null) {
-					LayoutInflater layoutInflater = (LayoutInflater) mContext
-							.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-					convertView = layoutInflater.inflate(mLayout, null);
-					views = new TextView[mTo.length];
-					for (int i = 0; i < mTo.length; i++) {
-						views[i] = (TextView) convertView.findViewById(mTo[i]);
-					}
-					convertView.setTag(views);
-				} else {
-					views = (TextView[]) convertView.getTag();
-				}
-
-				JSONObject item = getItem(position);
-				Log.d(TAG, "item=" + item.toString());
-
-				for (int i = 0; i < views.length; i++) {
-					try {
-						views[i].setText(item.getString(mFrom[i]));
-					} catch (JSONException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-
-				return convertView;
+			/**
+			 * Gets the view type for this item by the cursor. The cursor needs to be moved to the correct position prior to calling
+			 * this function
+			 * @param cursor
+			 * @return the view type
+			 */
+			public int getItemViewType(Cursor cursor) {
+				if("photo".equals(getItemPromptType(cursor)))
+					return IMAGE_RESPONSE;
+				else
+					return TEXT_RESPONSE;
 			}
 
-			public void setData(JSONArray data) {
-				mData = data;
-				this.notifyDataSetInvalidated();
+			/**
+			 * Gets the prompt type for this item by the cursor. The cursor needs to be moved to the correct position prior to calling
+			 * this function
+			 * @param cursor
+			 * @return
+			 */
+			public String getItemPromptType(Cursor cursor) {
+				return cursor.getString(cursor.getColumnIndex(SurveyPrompt.PROMPT_TYPE));
+			}
+
+			@Override
+			public View newView(Context context, Cursor cursor, ViewGroup parent) {
+				View view = super.newView(context, cursor, parent);
+				View image = view.findViewById(R.id.prompt_image_value);
+				View text = view.findViewById(R.id.prompt_text_value);
+				View value = view.findViewById(R.id.prompt_value);
+				switch(getItemViewType(cursor)) {
+					case IMAGE_RESPONSE:
+						image.setVisibility(View.VISIBLE);
+						text.setVisibility(View.GONE);
+						value.setTag(image);
+						value.setBackgroundResource(R.drawable.prompt_response_image_item_bg);
+						break;
+					case TEXT_RESPONSE:
+						image.setVisibility(View.GONE);
+						text.setVisibility(View.VISIBLE);
+						value.setTag(text);
+						break;
+				}
+				return view;
+			}
+
+			@Override
+			public int getViewTypeCount() {
+				// One view type for text, another for images
+				return 2;
+			}
+
+			@Override
+			public boolean areAllItemsEnabled() {
+				return false;
+			}
+
+			@Override
+			public boolean isEnabled(int position) {
+				return false;
+			}
+
+			@Override
+			public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+
+				if(cursor.getColumnName(columnIndex).equals(PromptResponse.PROMPT_VALUE)) {
+					String value = cursor.getString(columnIndex);
+
+					if(view.getTag() instanceof ImageView) {
+						String campaignUrn = cursor.getString(cursor.getColumnIndex(Response.CAMPAIGN_URN));
+						File photoDir = new File(PhotoPrompt.IMAGE_PATH + "/" + campaignUrn.replace(':', '_'));
+						File photo = new File(photoDir, value + ".jpg");
+						Bitmap img = BitmapFactory.decodeFile(photo.getAbsolutePath());
+
+						((ImageView) view.getTag()).setImageBitmap(img);
+					} else if(view.getTag() instanceof TextView) {
+						String prompt_type = getItemPromptType(cursor);
+						if("multi_choice".equals(prompt_type)) {
+							value = "¥ Choice number 1\n¥ Much more tired\n¥ I went to the park and played";
+						}
+						((TextView) view.getTag()).setText(value);
+					}
+					return true;
+				}
+
+				return false;
 			}
 		}
 	}
