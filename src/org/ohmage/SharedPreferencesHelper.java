52dba04a17abc8b16bd42790224d895d87dd5108
Cameron Ketcham <cketcham@gmail.com>
2011-11-03 15:18:05 -0700

    updated initial settings for NIH

--- src/org/ohmage/SharedPreferencesHelper.java
+++ src/org/ohmage/SharedPreferencesHelper.java
@@ -22,11 +22,10 @@ public class SharedPreferencesHelper {
 	
 	public static final String CLIENT_STRING = "android";
 
-//	public static final String DEFAULT_SERVER_URL = "https://dev1.andwellness.org/";
-	public static final String DEFAULT_SERVER_URL = "https://dev.mobilizingcs.org/";
-	public static final boolean IS_SINGLE_CAMPAIGN = false;
-	public static final boolean ALLOWS_FEEDBACK = true;
-	public static final boolean ADMIN_MODE = true;
+	public static final String DEFAULT_SERVER_URL = "https://dev.andwellness.org/";
+	public static final boolean IS_SINGLE_CAMPAIGN = true;
+	public static final boolean ALLOWS_FEEDBACK = false;
+	public static final boolean ADMIN_MODE = false;
 	
 	private static final String PREFERENCES_NAME = "preferences_name";
 	public static final String PREFERENCES_CREDENTIALS = "preferences_credentials";
