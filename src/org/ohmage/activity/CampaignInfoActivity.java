cens/ohmagePhone

commit 7c2f60e523d7579a8c04a5bce63bd74f747bbb69

fixed #183

src/org/ohmage/activity/CampaignInfoActivity.java
@@ -31,6 +31,8 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class CampaignInfoActivity extends BaseInfoActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+	private static final int TRIGGER_UPDATE_FINISHED = 0;
+
 	// helpers
 	private FragmentActivity mContext;
 	private SharedPreferencesHelper mSharedPreferencesHelper;
@@ -52,6 +54,8 @@ public class CampaignInfoActivity extends BaseInfoActivity implements LoaderMana
 	// state vars
 	private int mCampaignStatus; // status code for campaign as of last refresh
 	private int mLocalResponses;
+
+	private String mCampaignUrn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -115,7 +119,17 @@ public class CampaignInfoActivity extends BaseInfoActivity implements LoaderMana
 		// or start a new one.
 		getSupportLoaderManager().initLoader(0, null, this);
 	}
-	
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch(requestCode) {
+			case TRIGGER_UPDATE_FINISHED:
+				//Triggers might have changed so we update it here
+				setTriggerCount();
+				break;
+		}
+	}
+
 	protected void populateCommands(final String campaignUrn, final int campaignStatus) {
 		// first remove all the commands from the action bar...
 		ActionBarControl actionBar = getActionBar();
@@ -154,7 +168,8 @@ public class CampaignInfoActivity extends BaseInfoActivity implements LoaderMana
 								startActivity(intent);
 								break;
 							case ACTION_SETUP_TRIGGERS:
-								Campaign.launchTriggerActivity(CampaignInfoActivity.this, campaignUrn);
+								Intent triggerIntent = Campaign.launchTriggerActivity(mContext, campaignUrn);
+								startActivityForResult(triggerIntent, TRIGGER_UPDATE_FINISHED);
 								return;
 						}
 					}
@@ -280,11 +295,11 @@ public class CampaignInfoActivity extends BaseInfoActivity implements LoaderMana
 		if (!data.moveToFirst())
 			return;
 
-		String campaignUrn = data.getString(QueryParams.URN);
+		mCampaignUrn = data.getString(QueryParams.URN);
 
 		// set the header fields first
 		mHeadertext.setText(data.getString(QueryParams.NAME));
-		mSubtext.setText(campaignUrn);
+		mSubtext.setText(mCampaignUrn);
 		mNotetext.setVisibility(View.INVISIBLE);
 
 		final String iconUrl = data.getString(QueryParams.ICON);
@@ -349,30 +364,35 @@ public class CampaignInfoActivity extends BaseInfoActivity implements LoaderMana
 
 		// set the responses by querying the response table
 		// and getting the number of responses submitted for this campaign
-		Cursor responses = getContentResolver().query(Campaigns.buildResponsesUri(campaignUrn), null, null, null, null);
+		Cursor responses = getContentResolver().query(Campaigns.buildResponsesUri(mCampaignUrn), null, null, null, null);
 		mResponsesValue.setText(getResources().getQuantityString(R.plurals.campaign_info_response_count, responses.getCount(), responses.getCount()));
 
-		Cursor localResponses = getContentResolver().query(Campaigns.buildResponsesUri(campaignUrn), new String[] { Responses._ID },
+		Cursor localResponses = getContentResolver().query(Campaigns.buildResponsesUri(mCampaignUrn), new String[] { Responses._ID },
 				Responses.RESPONSE_STATUS + "!=" + Response.STATUS_DOWNLOADED + " AND " + Responses.RESPONSE_STATUS + "!=" + Response.STATUS_UPLOADED, null, null);
 		mLocalResponses = localResponses.getCount();
 
-		// get the number of triggers for this campaign
-		TriggerDB trigDB = new TriggerDB(mContext);
-		if (trigDB.open()) {
-			Cursor triggers = trigDB.getAllTriggers(campaignUrn);
-			mTriggersValue.setText(getResources().getQuantityString(R.plurals.campaign_info_trigger_count, triggers.getCount(), triggers.getCount()));
-			triggers.close();
-			trigDB.close();
-		}
+		// get the number of triggers for this survey
+		setTriggerCount();
 
 		// and finally populate the action bar + command tray
-		populateCommands(campaignUrn, mCampaignStatus);
+		populateCommands(mCampaignUrn, mCampaignStatus);
 
 		// and make the entity header visible (although i assume it already was)
 		mEntityHeader.setVisibility(View.VISIBLE);
 
 		// finally, show our content
 		setLoadingVisibility(false);
+	}
+
+	private void setTriggerCount() {
+		// get the number of triggers for this campaign
+		TriggerDB trigDB = new TriggerDB(mContext);
+		if (trigDB.open()) {
+			Cursor triggers = trigDB.getAllTriggers(mCampaignUrn);
+			mTriggersValue.setText(getResources().getQuantityString(R.plurals.campaign_info_trigger_count, triggers.getCount(), triggers.getCount()));
+			triggers.close();
+			trigDB.close();
+		}
 	}
 
 	@Override

src/org/ohmage/activity/SurveyInfoActivity.java
@@ -11,7 +11,6 @@ import org.ohmage.db.DbContract.Responses;
 import org.ohmage.db.DbContract.Surveys;
 import org.ohmage.db.Models.Campaign;
 import org.ohmage.triggers.base.TriggerDB;
-import org.ohmage.triggers.glue.TriggerFramework;
 import org.ohmage.ui.BaseInfoActivity;
 import org.ohmage.ui.OhmageFilterable.CampaignFilter;
 import org.ohmage.ui.OhmageFilterable.CampaignSurveyFilter;
@@ -31,10 +30,9 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class SurveyInfoActivity extends BaseInfoActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+	protected static final int TRIGGER_UPDATE_FINISHED = 0;
+
 	// action bar commands
 	private static final int ACTION_VIEW_RESPHISTORY = 1;
 	private static final int ACTION_SETUP_TRIGGERS = 2;
@@ -55,6 +53,9 @@ public class SurveyInfoActivity extends BaseInfoActivity implements LoaderManage
 	private int mCampaignStatus; // status code for campaign as of last refresh
 	private Handler mHandler;
 	private ContentObserver mResponsesObserver;
+
+	private String mSurveyTitle;
+	private String mCampaignUrn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -113,11 +114,15 @@ public class SurveyInfoActivity extends BaseInfoActivity implements LoaderManage
 		// or start a new one.
 		getSupportLoaderManager().initLoader(1, null, this);
 	}
-	
+
 	@Override
-	protected void onResume() {
-		// TODO Auto-generated method stub
-		super.onResume();
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch(requestCode) {
+			case TRIGGER_UPDATE_FINISHED:
+				//Triggers might have changed so we update it here
+				setTriggerCount();
+				break;
+		}
 	}
 	
 	protected void populateCommands(final String surveyID, final String campaignUrn, final String surveyTitle, final String surveySubmitText, int campaignStatus) {
@@ -148,16 +153,8 @@ public class SurveyInfoActivity extends BaseInfoActivity implements LoaderManage
 							startActivity(intent);
 							break;
 						case ACTION_SETUP_TRIGGERS:
-							List<String> surveyTitles = new ArrayList<String>();
-							
-							// grab a list of surveys for this campaign
-							Cursor surveys = getContentResolver().query(Campaigns.buildSurveysUri(campaignUrn), null, null, null, null);
-							
-							while (surveys.moveToNext()) {
-								surveyTitles.add(surveys.getString(surveys.getColumnIndex(Surveys.SURVEY_TITLE)));
-							}
-							
-							TriggerFramework.launchTriggersActivity(mContext, campaignUrn, surveyTitles.toArray(new String[surveyTitles.size()]));
+							Intent triggerIntent = Campaign.launchTriggerActivity(mContext, campaignUrn);
+							startActivityForResult(triggerIntent, TRIGGER_UPDATE_FINISHED);
 							return;
 					}
 				}
@@ -226,8 +223,8 @@ public class SurveyInfoActivity extends BaseInfoActivity implements LoaderManage
 
 		// populate the views
 		final String surveyID = data.getString(QueryParams.SURVEY_ID);
-		final String campaignUrn = data.getString(QueryParams.CAMPAIGN_URN);
-		String surveyTitle = data.getString(QueryParams.SURVEY_TITLE);
+		mSurveyTitle = data.getString(QueryParams.SURVEY_TITLE);
+		mCampaignUrn = data.getString(QueryParams.CAMPAIGN_URN);
 		String submitText = data.getString(QueryParams.SUBMIT_TEXT);
 
 		// set the header fields first
@@ -294,7 +291,7 @@ public class SurveyInfoActivity extends BaseInfoActivity implements LoaderManage
 
 				// set the responses by querying the response table
 				// and getting the number of responses submitted for this campaign
-				Cursor responses = getContentResolver().query(Campaigns.buildResponsesUri(campaignUrn, surveyID), null, null, null, null);
+				Cursor responses = getContentResolver().query(Campaigns.buildResponsesUri(mSurveyTitle, surveyID), null, null, null, null);
 				mResponsesValue.setText(getResources().getQuantityString(R.plurals.campaign_info_response_count, responses.getCount(), responses.getCount()));
 			}
 		};
@@ -305,22 +302,26 @@ public class SurveyInfoActivity extends BaseInfoActivity implements LoaderManage
 		mResponsesObserver.onChange(false);
 		
 		// get the number of triggers for this survey
-		TriggerDB trigDB = new TriggerDB(mContext);
-		if (trigDB.open()) {
-			Cursor triggers = trigDB.getSurveyTriggers(campaignUrn, surveyTitle);
-			mTriggersValue.setText(getResources().getQuantityString(R.plurals.campaign_info_trigger_count, triggers.getCount(), triggers.getCount()));
-			triggers.close();
-			trigDB.close();
-		}
+		setTriggerCount();
 		
 		// and finally populate the action bar + command tray
-		populateCommands(surveyID, campaignUrn, data.getString(QueryParams.TITLE), submitText, mCampaignStatus);
+		populateCommands(surveyID, mCampaignUrn, data.getString(QueryParams.TITLE), submitText, mCampaignStatus);
 
 		// and make the entity header visible (although i assume it already was)
 		mEntityHeader.setVisibility(View.VISIBLE);
 		
 		// finally, show our content
 		setLoadingVisibility(false);
+	}
+
+	public void setTriggerCount() {
+		TriggerDB trigDB = new TriggerDB(mContext);
+		if (trigDB.open()) {
+			Cursor triggers = trigDB.getSurveyTriggers(mCampaignUrn, mSurveyTitle);
+			mTriggersValue.setText(getResources().getQuantityString(R.plurals.campaign_info_trigger_count, triggers.getCount(), triggers.getCount()));
+			triggers.close();
+			trigDB.close();
+		}
 	}
 
 	@Override

src/org/ohmage/db/Models.java
@@ -11,6 +11,7 @@ import org.ohmage.triggers.glue.TriggerFramework;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 
 import java.io.File;
@@ -108,7 +109,7 @@ public class Models {
 		 * @param context
 		 * @param campaignUrn
 		 */
-		public static void launchTriggerActivity(Context context, String campaignUrn) {
+		public static Intent launchTriggerActivity(Context context, String campaignUrn) {
 			List<String> surveyTitles = new ArrayList<String>();
 			
 			// grab a list of surveys for this campaign
@@ -118,7 +119,7 @@ public class Models {
 				surveyTitles.add(surveys.getString(surveys.getColumnIndex(Surveys.SURVEY_TITLE)));
 			}
 			
-			TriggerFramework.launchTriggersActivity(context, campaignUrn, surveyTitles.toArray(new String[surveyTitles.size()]));
+			return TriggerFramework.launchTriggersIntent(context, campaignUrn, surveyTitles.toArray(new String[surveyTitles.size()]));
 		}
 
 		/**

src/org/ohmage/triggers/glue/TriggerFramework.java
@@ -58,12 +58,12 @@ public class TriggerFramework {
 	 * is the entry point to the trigger framework. The list of all surveys 
 	 * for which any trigger is to be set must be passed as argument.
 	 */
-	public static void launchTriggersActivity(Context context, String campaignUrn, String[] surveys ) {
+	public static Intent launchTriggersIntent(Context context, String campaignUrn, String[] surveys ) {
 		
 		Intent i = new Intent(context, TriggerListActivity.class);
 		i.putExtra(TriggerListActivity.KEY_CAMPAIGN_URN, campaignUrn);
 		i.putExtra(TriggerListActivity.KEY_ACTIONS, surveys);
-		context.startActivity(i);
+		return i;
 	}
 	
 	/*

commit 50f56304a1704979f2c6fdb40ffbad104723a797

fixed bug caused by 7c2f60e523d7579a8c04a5bce63bd74f747bbb69 which caused the trigger settings button to not launch the trigger activity

src/org/ohmage/activity/CampaignInfoActivity.java
@@ -168,7 +168,7 @@ public class CampaignInfoActivity extends BaseInfoActivity implements LoaderMana
 								startActivity(intent);
 								break;
 							case ACTION_SETUP_TRIGGERS:
-								Intent triggerIntent = Campaign.launchTriggerActivity(mContext, campaignUrn);
+								Intent triggerIntent = Campaign.launchTriggerIntent(mContext, campaignUrn);
 								startActivityForResult(triggerIntent, TRIGGER_UPDATE_FINISHED);
 								return;
 						}

src/org/ohmage/activity/OhmagePreferenceActivity.java
@@ -47,9 +47,10 @@ public class OhmagePreferenceActivity extends PreferenceActivity  {
 				@Override
 				public boolean onPreferenceClick(Preference preference) {
 					String urn = Campaign.getSingleCampaign(OhmagePreferenceActivity.this);
-					if(!TextUtils.isEmpty(urn))
-						Campaign.launchTriggerActivity(OhmagePreferenceActivity.this, Campaign.getSingleCampaign(OhmagePreferenceActivity.this));
-					else
+					if(!TextUtils.isEmpty(urn)) {
+						Intent triggers = Campaign.launchTriggerIntent(OhmagePreferenceActivity.this, Campaign.getSingleCampaign(OhmagePreferenceActivity.this));
+						startActivity(triggers);
+					} else
 						Toast.makeText(OhmagePreferenceActivity.this, R.string.preferences_no_single_campaign, Toast.LENGTH_LONG).show();
 					return true;
 				}

src/org/ohmage/activity/SurveyInfoActivity.java
@@ -153,7 +153,7 @@ public class SurveyInfoActivity extends BaseInfoActivity implements LoaderManage
 							startActivity(intent);
 							break;
 						case ACTION_SETUP_TRIGGERS:
-							Intent triggerIntent = Campaign.launchTriggerActivity(mContext, campaignUrn);
+							Intent triggerIntent = Campaign.launchTriggerIntent(mContext, campaignUrn);
 							startActivityForResult(triggerIntent, TRIGGER_UPDATE_FINISHED);
 							return;
 					}

src/org/ohmage/db/Models.java
@@ -109,7 +109,7 @@ public class Models {
 		 * @param context
 		 * @param campaignUrn
 		 */
-		public static Intent launchTriggerActivity(Context context, String campaignUrn) {
+		public static Intent launchTriggerIntent(Context context, String campaignUrn) {
 			List<String> surveyTitles = new ArrayList<String>();
 			
 			// grab a list of surveys for this campaign