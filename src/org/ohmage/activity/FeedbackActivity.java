88343b531ca57f509db2f5b4bd5829b53676fa91
Cameron Ketcham <cketcham@gmail.com>
2011-12-16 15:51:33 -0800

    added real data for NIH to the feedback activity

--- src/org/ohmage/activity/FeedbackActivity.java
+++ src/org/ohmage/activity/FeedbackActivity.java
@@ -3,61 +3,66 @@ package org.ohmage.activity;
 
 import org.ohmage.ChartFragment;
 import org.ohmage.R;
-import org.ohmage.Utilities;
 import org.ohmage.charts.Histogram;
-import org.ohmage.charts.SparkLine;
 import org.ohmage.db.DbContract.Responses;
+import org.ohmage.fragments.RecentChartFragment;
 import org.ohmage.ui.BaseActivity;
 
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.text.format.DateUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import java.util.Arrays;
 
 public class FeedbackActivity extends BaseActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
-	public static final double[] FAKE_DATA1 = new double[] {
-			1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0
-	};
-
-	public static final double[] FAKE_DATA2 = new double[] {
-			1, 1, 1, 1, 1, 1, 5, 5, 5, 5, 5, 5, 5, 7, 7, 7, 7, 7, 7, 5, 5, 5, 5, 5, 5
-	};
-
 	private static final String TAG = "FeedbackActivity";
 
-	private static final int LOADER_RESPONSE_HISTOGRAM = 0;
+	/**
+	 * true when the charts have been loaded
+	 */
+	private boolean mChartsLoaded;
+
+	/**
+	 * true when the responses have been loaded
+	 */
+	private boolean mResponsesLoaded;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.feedback_layout);
 
-		if (getSupportFragmentManager().findFragmentById(R.id.chart1) == null) {
-			setChart("Exercise Frequency", FAKE_DATA1, R.id.chart1_title, R.id.chart1, R.color.powderkegblue, R.color.light_blue);
-		}
+		if (getSupportFragmentManager().findFragmentById(R.id.feedback_chart_container) == null) {
+			Fragment chartFragment = new RecentChartFragment() {
 
-		if (getSupportFragmentManager().findFragmentById(R.id.chart2) == null) {
-			setChart("Healthy Diet", FAKE_DATA2, R.id.chart2_title, R.id.chart2, R.color.dark_green, R.color.light_green);
-		}
+				@Override
+				public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+					super.onLoadFinished(loader, data);
+					mChartsLoaded = true;
+					maybeShowContent();
+				}
+			};
+			getSupportFragmentManager().beginTransaction().add(R.id.feedback_chart_container, chartFragment).commit();
 
-		if (getSupportFragmentManager().findFragmentById(R.id.chart3) == null) {
-			setChart("Random Graph", Utilities.randomData(30, 9), R.id.chart3_title, R.id.chart3, R.color.dark_purple, R.color.light_purple);
 		}
 
 		if (getSupportFragmentManager().findFragmentById(R.id.feedback_response_graph) == null) {
 			ChartFragment f = ChartFragment.newInstance();
 			getSupportFragmentManager().beginTransaction().add(R.id.feedback_response_graph, f).commit();
-			getSupportLoaderManager().initLoader(LOADER_RESPONSE_HISTOGRAM, null, this);
+			getSupportLoaderManager().initLoader(0, null, this);
 		}
+	}
+
+	private void maybeShowContent() {
+		setLoadingVisibility(!mChartsLoaded || !mResponsesLoaded);
 	}
 
 	@Override
@@ -82,14 +87,6 @@ public class FeedbackActivity extends BaseActivity implements LoaderManager.Load
 		});
 
 		setLoadingVisibility(true);
-	}
-
-	private void setChart(String title, double[] data, int chartTitleId, int chartId, int colorId, int fillColorId) {
-		SparkLine chart = new SparkLine(this, data, getResources().getColor(colorId), getResources().getColor(fillColorId));
-		chart.getRenderer().setInScroll(true);
-		((TextView) findViewById(chartTitleId)).setText(title);
-		ChartFragment f = ChartFragment.newInstance(chart);
-		getSupportFragmentManager().beginTransaction().add(chartId, f).commit();
 	}
 
 	@Override
@@ -128,7 +125,7 @@ public class FeedbackActivity extends BaseActivity implements LoaderManager.Load
 		ChartFragment chart = (ChartFragment) getSupportFragmentManager().findFragmentById(R.id.feedback_response_graph);
 		chart.setChart(new Histogram(this, renderer, values));
 
-		setLoadingVisibility(false);
+		mResponsesLoaded = true;
+		maybeShowContent();
 	}
-
 }
