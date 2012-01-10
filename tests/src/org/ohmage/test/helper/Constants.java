ec06331e80f12574d612c222983ddea08289ac6c
Cameron Ketcham <cketcham@gmail.com>
2012-01-09 18:40:03 -0800

    added tests for the response history activity

---
+++ tests/src/org/ohmage/test/helper/Constants.java
@@ -0,0 +1,7 @@
+package org.ohmage.test.helper;
+
+public class Constants {
+	public static final String MONTH_CURRENT = "January 2012";
+	public static final String MONTH_NEXT = "February 2012";
+	public static final String MONTH_PREVIOUS = "December 2011";
+}
