333671fba25c597037d55be5fb6e6674c59aa262
Cameron Ketcham <cketcham@gmail.com>
2012-02-23 15:34:31 -0800

    fixed #286. A secure pincode has been added when not in admin mode

--- src/org/ohmage/fragments/AdminDialogFragment.java
+++ src/org/ohmage/fragments/AdminDialogFragment.java
@@ -1,7 +1,6 @@
 package org.ohmage.fragments;
 
 import org.ohmage.R;
-import org.ohmage.triggers.config.TrigUserConfig;
 import org.ohmage.triggers.utils.TrigTextInput;
 
 import android.app.Activity;
@@ -9,13 +8,22 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.util.Base64;
 import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class AdminDialogFragment extends DialogFragment {
 
 	public interface AdminCodeListener {
 		public void onAdminCodeEntered(boolean success);
 	}
+
+	protected static final String ADMIN_CODE = "buZMBBBnFVSjOFZYlr3Dgt42H6U3dcfU73RsS08bSKY=\n";
+
+	protected static final String SALT = "ohmage";
 
 	private AdminCodeListener mListener;
 	private boolean mSuccess = false;
@@ -53,9 +61,19 @@ public class AdminDialogFragment extends DialogFragment {
 			@Override
 			public void onClick(TrigTextInput ti, int which) {
 				if (which == TrigTextInput.BUTTON_POSITIVE) {
-					if(ti.getText().equals(TrigUserConfig.adminPass)) {
-						mSuccess = true;
-					} else {
+					try {
+						MessageDigest digest = MessageDigest.getInstance("SHA-256");
+						digest.reset();
+						digest.update(SALT.getBytes());
+						byte[] input = digest.digest(String.valueOf(ti.getText()).getBytes("UTF-8"));
+
+						mSuccess = MessageDigest.isEqual(Base64.encode(input, 0), ADMIN_CODE.getBytes());
+					} catch (NoSuchAlgorithmException e) {
+						throw new RuntimeException(e);
+					} catch (UnsupportedEncodingException e) {
+						e.printStackTrace();
+					}
+					if(!mSuccess) {
 						Toast.makeText(getActivity(), R.string.admin_dialog_invalid_pin, Toast.LENGTH_SHORT).show();
 					}
 				}
