cb4706d1f9c6abbc4d0798ac391a291c2be87009
Cameron Ketcham <cketcham@gmail.com>
2012-02-23 18:02:06 -0800

    fixed #287. The reminders admin mode is now separate from the main admin mode

--- config/Config.java
+++ config/Config.java
@@ -6,6 +6,7 @@ public class Config {
 	public static final boolean IS_SINGLE_CAMPAIGN = @CONFIG.SINGLE_CAMPAIGN@;
 	public static final boolean ALLOWS_FEEDBACK = @CONFIG.ALLOWS_FEEDBACK@;
 	public static final boolean ADMIN_MODE = @CONFIG.ADMIN_MODE@;
+	public static final boolean REMINDER_ADMIN_MODE = @CONFIG.REMINDER_ADMIN_MODE@;
 	public static final boolean LOG_ANALYTICS = @CONFIG.LOG_ANALYTICS@;
 	public static final String LOG_LEVEL = @CONFIG.LOG_LEVEL@;
 
--- src/org/ohmage/triggers/ui/TriggerListActivity.java
+++ src/org/ohmage/triggers/ui/TriggerListActivity.java
@@ -107,12 +107,7 @@ public class TriggerListActivity extends ListActivity
 	/**
 	 * Instead of having a shared preference admin mode, we want admin mode to end once the user leaves the activity
 	 */
-	private boolean mAdminMode = TRIGGER_ADMIN_MODE;
-
-	/**
-	 * Set the default admin mode. If it is true, we don't need to show the admin menu
-	 */
-	public static boolean TRIGGER_ADMIN_MODE = Config.ADMIN_MODE;
+	private boolean mAdminMode = Config.REMINDER_ADMIN_MODE;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -567,7 +562,7 @@ public class TriggerListActivity extends ListActivity
 		menu.removeItem(MENU_ID_SETTINGS);
 		
 		boolean adminMode = isAdminLoggedIn();
-		if(!TRIGGER_ADMIN_MODE) {
+		if(!Config.REMINDER_ADMIN_MODE) {
 			if(!adminMode) {
 				menu.add(0, MENU_ID_ADMIN_LOGIN, 0, R.string.trigger_menu_admin_turn_on)
 				.setIcon(R.drawable.ic_menu_login);
