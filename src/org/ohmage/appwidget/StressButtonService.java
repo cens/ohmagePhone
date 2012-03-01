02f6d9c532620edc8e4bff8ed0a93edb264bd1f5
Cameron Ketcham <cketcham@gmail.com>
2012-02-29 17:35:20 -0800

    changed the stress button to use the storeResponse provided by the SurveyActivity

--- src/org/ohmage/appwidget/StressButtonService.java
+++ src/org/ohmage/appwidget/StressButtonService.java
@@ -17,41 +17,29 @@ package org.ohmage.appwidget;
 
 import edu.ucla.cens.systemlog.Log;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.ohmage.CampaignXmlHelper;
 import org.ohmage.Config;
 import org.ohmage.OhmageApplication;
 import org.ohmage.PromptXmlParser;
 import org.ohmage.SharedPreferencesHelper;
 import org.ohmage.activity.LoginActivity;
-import org.ohmage.db.DbContract.Responses;
+import org.ohmage.activity.SurveyActivity;
 import org.ohmage.db.Models.Campaign;
-import org.ohmage.db.Models.Response;
 import org.ohmage.prompt.AbstractPrompt;
 import org.ohmage.prompt.SurveyElement;
-import org.ohmage.service.SurveyGeotagService;
-import org.ohmage.triggers.glue.TriggerFramework;
 import org.xmlpull.v1.XmlPullParserException;
 
 import android.app.IntentService;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources.NotFoundException;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Handler;
 import android.os.Vibrator;
 import android.widget.Toast;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
-import java.util.TimeZone;
-import java.util.UUID;
 
 public class StressButtonService extends IntentService {
 	
@@ -111,16 +99,14 @@ public class StressButtonService extends IntentService {
 		}
 		
 		if (prompts != null && prompts.size() > 0) {
-		
-			startService(new Intent(this, SurveyGeotagService.class));
-	
+
 			if (((AbstractPrompt)prompts.get(0)).getResponseObject() == null) {
 				mHandler.post(new DisplayToast("There is a bug: default value not being set!"));
 			} else {
 				((AbstractPrompt)prompts.get(0)).setDisplayed(true);
 				((AbstractPrompt)prompts.get(0)).setSkipped(false);
 				Log.i(TAG, ((AbstractPrompt)prompts.get(0)).getResponseJson());
-				storeResponse(campaignUrn, surveyId, surveyTitle, launchTime, prompts);
+				SurveyActivity.storeResponse(this, surveyId, launchTime, campaignUrn, surveyTitle, prompts);
 				//Toast.makeText(this, "Registered stressful event.", Toast.LENGTH_SHORT).show();
 				mHandler.post(new DisplayToast("Registered stressful event!"));
 				
@@ -155,87 +141,5 @@ public class StressButtonService extends IntentService {
 			
 			Toast.makeText(getApplicationContext(), mText, Toast.LENGTH_SHORT).show();
 		}
-	}
-
-	private void storeResponse(String campaignUrn, String surveyId, String surveyTitle, long launchTime, List<SurveyElement> prompts) {
-		
-		SharedPreferencesHelper helper = new SharedPreferencesHelper(this);
-		String username = helper.getUsername();
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		Calendar now = Calendar.getInstance();
-		String date = dateFormat.format(now.getTime());
-		long time = now.getTimeInMillis();
-		String timezone = TimeZone.getDefault().getID();
-		
-		LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-		Location loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-		if (loc == null || System.currentTimeMillis() - loc.getTime() > SurveyGeotagService.LOCATION_STALENESS_LIMIT || loc.getAccuracy() > SurveyGeotagService.LOCATION_ACCURACY_THRESHOLD) {
-			Log.w(TAG, "gps provider disabled or location stale or inaccurate");
-			loc = null;
-		}
-		
-		//get launch context from trigger glue
-		JSONObject surveyLaunchContextJson = new JSONObject();
-		try {
-			surveyLaunchContextJson.put("launch_time", launchTime);
-			surveyLaunchContextJson.put("launch_timezone", timezone);
-			surveyLaunchContextJson.put("active_triggers", TriggerFramework.getActiveTriggerInfo(this, campaignUrn, surveyTitle));
-		} catch (JSONException e1) {
-			throw new RuntimeException(e1);
-		}
-		String surveyLaunchContext = surveyLaunchContextJson.toString();
-		
-		JSONArray responseJson = new JSONArray();
-		for (int i = 0; i < prompts.size(); i++) {
-			JSONObject itemJson = new JSONObject();
-			try {
-				itemJson.put("prompt_id", ((AbstractPrompt)prompts.get(i)).getId());
-				itemJson.put("value", ((AbstractPrompt)prompts.get(i)).getResponseObject());
-			} catch (JSONException e) {
-				throw new RuntimeException(e);
-			}
-			responseJson.put(itemJson);
-		}
-		String response = responseJson.toString();
-		
-//		DbHelper dbHelper = new DbHelper(this);
-//		if (loc != null) {
-//			dbHelper.addResponseRow(campaign, campaignVersion, username, date, time, timezone, SurveyGeotagService.LOCATION_VALID, loc.getLatitude(), loc.getLongitude(), loc.getProvider(), loc.getAccuracy(), loc.getTime(), surveyId, surveyLaunchContext, response);
-//		} else {
-//			dbHelper.addResponseRowWithoutLocation(campaign, campaignVersion, username, date, time, timezone, surveyId, surveyLaunchContext, response);
-//		}
-		
-		Response candidate = new Response();
-		
-		candidate.uuid = UUID.randomUUID().toString();
-		candidate.campaignUrn = campaignUrn;
-		candidate.username = username;
-		candidate.date = date;
-		candidate.time = time;
-		candidate.timezone = timezone;
-		candidate.surveyId = surveyId;
-		candidate.surveyLaunchContext = surveyLaunchContext;
-		candidate.response = response;
-		candidate.status = Response.STATUS_STANDBY;
-		
-		if (loc != null) {
-			candidate.locationStatus = SurveyGeotagService.LOCATION_VALID;
-			candidate.locationLatitude = loc.getLatitude();
-			candidate.locationLongitude = loc.getLongitude();
-			candidate.locationProvider = loc.getProvider();
-			candidate.locationAccuracy = loc.getAccuracy();
-			candidate.locationTime = loc.getTime();
-		} else {
-			candidate.locationStatus = SurveyGeotagService.LOCATION_UNAVAILABLE;
-			candidate.locationLatitude = -1;
-			candidate.locationLongitude = -1;
-			candidate.locationProvider = null;
-			candidate.locationAccuracy = -1;
-			candidate.locationTime = -1;
-			candidate.status = Response.STATUS_WAITING_FOR_LOCATION;
-		}
-
-		ContentResolver cr = getContentResolver();
-		cr.insert(Responses.CONTENT_URI, candidate.toCV());
 	}
 }
