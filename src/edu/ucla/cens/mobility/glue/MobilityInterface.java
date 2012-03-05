028b1706598f4df33f7d316fa2b0755181cd6df2
Cameron Ketcham <cketcham@gmail.com>
2012-03-05 11:36:28 -0800

    removed unused code from MobilityInterface

--- src/edu/ucla/cens/mobility/glue/MobilityInterface.java
+++ src/edu/ucla/cens/mobility/glue/MobilityInterface.java
@@ -1,15 +1,12 @@
 package edu.ucla.cens.mobility.glue;
 
-import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
-import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
-import android.widget.Toast;
 
-public class MobilityInterface
-{
+public class MobilityInterface {
+
 	public static final String KEY_MODE = "mode";
 	public static final String KEY_ID = "id";
 	public static final String KEY_SPEED = "speed";
@@ -25,38 +22,19 @@ public class MobilityInterface
 	public static final String KEY_LATITUDE = "latitude";
 	public static final String KEY_LONGITUDE = "longitude";
 	private static String [] columns = {KEY_ROWID, KEY_ID, KEY_MODE, KEY_SPEED, KEY_STATUS, KEY_LOC_TIMESTAMP, KEY_ACCURACY, KEY_PROVIDER, KEY_WIFIDATA, KEY_ACCELDATA, KEY_TIME, KEY_TIMEZONE, KEY_LATITUDE, KEY_LONGITUDE};
+
 	// Content provider strings
 	public static final String AUTHORITY = "edu.ucla.cens.mobility.MobilityContentProvider";
 	public static final String PATH_MOBILITY = "mobility";
 	public static final Uri CONTENT_URI = Uri.parse("content://"+AUTHORITY + "/" + PATH_MOBILITY);
+
 	/**
 	 * Helper function to get cursor to data with only the last retrieved timestamp.
 	 * @param timestamp
 	 * @return cursor to the data after the timestamp
 	 */
-	public static Cursor getMobilityCursor(Context context, Long timestamp)
-	{
+	public static Cursor getMobilityCursor(Context context, Long timestamp) {
 		ContentResolver r = context.getContentResolver();
-		
 		return r.query(CONTENT_URI, columns, KEY_TIME + " > ?", new String[] {String.valueOf(timestamp)}, KEY_TIME);
 	}
-	
-	
-	public static void showMobilityOptions(Context context)
-	{
-//		context.startActivity(new Intent(context, MobilityControl.class));
-		try
-		{
-			final Intent intentDeviceTest = new Intent("android.intent.action.MAIN");                
-			intentDeviceTest.setComponent(new ComponentName("edu.ucla.cens.mobility","edu.ucla.cens.mobility.MobilityControl"));
-			context.startActivity(intentDeviceTest);
-		}
-		catch(Exception e)
-		{
-			Toast.makeText(context, "There was an error. Please verify that Mobility has been installed.", Toast.LENGTH_SHORT).show();
-		}
-	}
-	
-	
-	
 }
