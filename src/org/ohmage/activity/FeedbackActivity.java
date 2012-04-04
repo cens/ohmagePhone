3ac0bdd4a7fff9698f55cd08b4780060332d41d6
Cameron Ketcham <cketcham@gmail.com>
2012-04-03 18:51:15 -0700

    moved the recent responses histogram into a fragment

--- src/org/ohmage/activity/FeedbackActivity.java
+++ src/org/ohmage/activity/FeedbackActivity.java
@@ -1,7 +1,7 @@
 
 package org.ohmage.activity;
 
-import org.ohmage.ChartFragment;
+import org.achartengine.GraphicalView;
 import org.ohmage.R;
 import org.ohmage.charts.Histogram;
 import org.ohmage.charts.HistogramBase.HistogramRenderer;
@@ -19,18 +19,18 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.text.format.DateUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
-public class FeedbackActivity extends BaseActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+public class FeedbackActivity extends BaseActivity {
 
 	private static final String TAG = "FeedbackActivity";
-
-	private static final int MAX_DATA_COLUMNS = 10;
 
 	/**
 	 * true when the charts have been loaded
@@ -62,7 +62,6 @@ public class FeedbackActivity extends BaseActivity implements LoaderManager.Load
 				}
 			};
 			getSupportFragmentManager().beginTransaction().add(R.id.feedback_compare_container, compareFragment).commit();
-
 		}
 
 		if (getSupportFragmentManager().findFragmentById(R.id.feedback_chart_container) == null) {
@@ -75,13 +74,18 @@ public class FeedbackActivity extends BaseActivity implements LoaderManager.Load
 				}
 			};
 			getSupportFragmentManager().beginTransaction().add(R.id.feedback_chart_container, chartFragment).commit();
-
 		}
 
-		if (getSupportFragmentManager().findFragmentById(R.id.feedback_response_graph) == null) {
-			ChartFragment f = ChartFragment.newInstance();
-			getSupportFragmentManager().beginTransaction().add(R.id.feedback_response_graph, f).commit();
-			getSupportLoaderManager().initLoader(0, null, this);
+		if (getSupportFragmentManager().findFragmentById(R.id.feedback_responses_container) == null) {
+			Fragment chartFragment = new RecentResponsesFragment() {
+				@Override
+				public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+					super.onLoadFinished(loader, data);
+					mResponsesLoaded = true;
+					maybeShowContent();
+				}
+			};
+			getSupportFragmentManager().beginTransaction().add(R.id.feedback_responses_container, chartFragment).commit();
 		}
 	}
 
@@ -92,68 +96,93 @@ public class FeedbackActivity extends BaseActivity implements LoaderManager.Load
 	@Override
 	public void onContentChanged() {
 		super.onContentChanged();
-
-		Button b = (Button) findViewById(R.id.feedback_response_history_calendar);
-		b.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(FeedbackActivity.this, ResponseHistoryActivity.class));
-			}
-		});
-		b = (Button) findViewById(R.id.feedback_response_history_map);
-		b.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(FeedbackActivity.this, ResponseHistoryActivity.class).putExtra(ResponseHistoryActivity.EXTRA_SHOW_MAP, true));
-			}
-		});
-
 		setLoadingVisibility(true);
 	}
 
-	@Override
-	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-		return new CursorLoader(FeedbackActivity.this, Responses.CONTENT_URI,
-				new String[] { Responses.RESPONSE_TIME },
-				Responses.RESPONSE_TIME + " < " + System.currentTimeMillis(), null, Responses.RESPONSE_TIME + " DESC");
-	}
+	public static class RecentResponsesFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
-	@Override
-	public void onLoaderReset(Loader<Cursor> loader) {
-		// Nothing to do?
-	}
+		private static final int MAX_DATA_COLUMNS = 10;
 
-	@Override
-	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-		List<FeedbackItem> values = new LinkedList<FeedbackItem>();
+		private static final int LOAD_RESPONSES = 0;
 
-		for(int i=0;i<MAX_DATA_COLUMNS;i++) {
-			FeedbackItem point = new FeedbackItem();
-			point.time = System.currentTimeMillis() - DateUtils.DAY_IN_MILLIS * i;
-			point.value = 0.0;
-			// Count up the data for each day
-			while(data.moveToNext() && DateUtils.isToday(data.getLong(0) + DateUtils.DAY_IN_MILLIS * i)) {
-				point.value++;
-			}
-			values.add(point);
-			data.moveToPrevious();
+		private ViewGroup mContainer;
+
+		@Override
+		public void onActivityCreated(Bundle savedInstanceState) {
+			super.onActivityCreated(savedInstanceState);
+			getLoaderManager().initLoader(LOAD_RESPONSES, null, this);
 		}
 
-		HistogramRenderer renderer = new HistogramRenderer(FeedbackActivity.this);
-		renderer.setMargins(new int[] {
-				30, 35, 15, 30
-		});
-		renderer.setYLabels(5);
-		renderer.setYAxisMin(0);
-		renderer.setYTitle("# of Responses");
-		renderer.setInScroll(true);
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View view = inflater.inflate(R.layout.recent_responses_fragment_layout, container, false);
 
-		ChartFragment chart = (ChartFragment) getSupportFragmentManager().findFragmentById(R.id.feedback_response_graph);
-		chart.setChart(new Histogram(this, renderer, values));
+			mContainer = (ViewGroup) view.findViewById(R.id.feedback_response_graph);
+			Button b = (Button) view.findViewById(R.id.feedback_response_history_calendar);
+			b.setOnClickListener(new View.OnClickListener() {
 
-		mResponsesLoaded = true;
-		maybeShowContent();
+				@Override
+				public void onClick(View v) {
+					startActivity(new Intent(getActivity(), ResponseHistoryActivity.class));
+				}
+			});
+			b = (Button) view.findViewById(R.id.feedback_response_history_map);
+			b.setOnClickListener(new View.OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					startActivity(new Intent(getActivity(), ResponseHistoryActivity.class).putExtra(ResponseHistoryActivity.EXTRA_SHOW_MAP, true));
+				}
+			});
+
+			return view;
+		}
+
+
+		@Override
+		public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+			return new CursorLoader(getActivity(), Responses.CONTENT_URI,
+					new String[] { Responses.RESPONSE_TIME },
+					Responses.RESPONSE_TIME + " < " + System.currentTimeMillis(), null, Responses.RESPONSE_TIME + " DESC");
+		}
+
+		@Override
+		public void onLoaderReset(Loader<Cursor> loader) {
+			// Nothing to do?
+		}
+
+		@Override
+		public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+			List<FeedbackItem> values = new LinkedList<FeedbackItem>();
+
+			// Move to the beginning of the data
+			data.moveToPosition(-1);
+
+			for (int i = 0; i < MAX_DATA_COLUMNS; i++) {
+				FeedbackItem point = new FeedbackItem();
+				point.time = System.currentTimeMillis() - DateUtils.DAY_IN_MILLIS * i;
+				point.value = 0.0;
+				// Count up the data for each day
+				while (data.moveToNext()
+						&& DateUtils.isToday(data.getLong(0) + DateUtils.DAY_IN_MILLIS * i)) {
+					point.value++;
+				}
+				values.add(point);
+				data.moveToPrevious();
+			}
+
+			HistogramRenderer renderer = new HistogramRenderer(getActivity());
+			renderer.setMargins(new int[] {
+					30, 35, 15, 30
+			});
+			renderer.setYLabels(5);
+			renderer.setYAxisMin(0);
+			renderer.setYTitle("# of Responses");
+			renderer.setInScroll(true);
+
+			mContainer.removeAllViews();
+			mContainer.addView(new GraphicalView(getActivity(), new Histogram(getActivity(), renderer, values)));
+		}
 	}
 }
