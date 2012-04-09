c33a761fc7c4e29ba456dccea5d47a2027c4ddce
Cameron Ketcham <cketcham@gmail.com>
2012-04-09 16:04:59 -0700

    updated mobility interface so its up to date

--- src/edu/ucla/cens/mobility/glue/MobilityInterface.java
+++ src/edu/ucla/cens/mobility/glue/MobilityInterface.java
@@ -1,12 +1,15 @@
 package edu.ucla.cens.mobility.glue;
 
+import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.widget.Toast;
 
-public class MobilityInterface {
-
+public class MobilityInterface
+{
 	public static final String KEY_MODE = "mode";
 	public static final String KEY_ID = "id";
 	public static final String KEY_SPEED = "speed";
@@ -22,11 +25,15 @@ public class MobilityInterface {
 	public static final String KEY_LATITUDE = "latitude";
 	public static final String KEY_LONGITUDE = "longitude";
 	private static String [] columns = {KEY_ROWID, KEY_ID, KEY_MODE, KEY_SPEED, KEY_STATUS, KEY_LOC_TIMESTAMP, KEY_ACCURACY, KEY_PROVIDER, KEY_WIFIDATA, KEY_ACCELDATA, KEY_TIME, KEY_TIMEZONE, KEY_LATITUDE, KEY_LONGITUDE};
-
 	// Content provider strings
 	public static final String AUTHORITY = "edu.ucla.cens.mobility.MobilityContentProvider";
 	public static final String PATH_MOBILITY = "mobility";
 	public static final Uri CONTENT_URI = Uri.parse("content://"+AUTHORITY + "/" + PATH_MOBILITY);
+
+	public static final String PATH_AGGREGATES = PATH_MOBILITY + "/" + "aggregates";
+	public static final Uri AGGREGATES_URI = Uri.parse("content://"+AUTHORITY + "/" + PATH_AGGREGATES);
+	public static final String KEY_DAY = "day";
+	public static final String KEY_COUNT = "count";
 
 	/**
 	 * Helper function to get cursor to data with only the last retrieved timestamp.
@@ -36,5 +43,19 @@ public class MobilityInterface {
 	public static Cursor getMobilityCursor(Context context, Long timestamp) {
 		ContentResolver r = context.getContentResolver();
 		return r.query(CONTENT_URI, columns, KEY_TIME + " > ?", new String[] {String.valueOf(timestamp)}, KEY_TIME);
+	}
+
+	public static void showMobilityOptions(Context context) {
+//		context.startActivity(new Intent(context, MobilityControl.class));
+		try
+		{
+			final Intent intentDeviceTest = new Intent("android.intent.action.MAIN");
+			intentDeviceTest.setComponent(new ComponentName("edu.ucla.cens.mobility","edu.ucla.cens.mobility.MobilityControl"));
+			context.startActivity(intentDeviceTest);
+		}
+		catch(Exception e)
+		{
+			Toast.makeText(context, "There was an error. Please verify that Mobility has been installed.", Toast.LENGTH_SHORT).show();
+		}
 	}
 }
