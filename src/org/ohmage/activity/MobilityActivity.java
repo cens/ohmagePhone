0ac7c612afd62d6304a8d5fbf55d90ac37c14716
Cameron Ketcham <cketcham@gmail.com>
2012-04-20 18:52:37 -0700

    added inital mobility analytics. re #315. fixed #331.

--- src/org/ohmage/activity/MobilityActivity.java
+++ src/org/ohmage/activity/MobilityActivity.java
@@ -1,93 +1,46 @@
 package org.ohmage.activity;
 
-import com.commonsware.cwac.wakeful.WakefulIntentService;
-
-import edu.ucla.cens.mobility.glue.IMobility;
-import edu.ucla.cens.mobility.glue.MobilityInterface;
-import edu.ucla.cens.systemlog.Analytics;
-import edu.ucla.cens.systemlog.Log;
 
 import org.ohmage.R;
-import org.ohmage.SharedPreferencesHelper;
-import org.ohmage.db.DbContract.Responses;
-import org.ohmage.service.UploadService;
+import org.ohmage.fragments.MobilityControlFragment;
+import org.ohmage.fragments.RecentMobilityChartFragment;
 import org.ohmage.ui.BaseActivity;
+import org.ohmage.ui.TabsAdapter;
 
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.ServiceConnection;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.os.RemoteException;
-import android.support.v4.app.LoaderManager.LoaderCallbacks;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
-import android.support.v4.widget.SimpleCursorAdapter;
-import android.text.format.DateFormat;
+import android.support.v4.view.ViewPager;
 import android.view.Gravity;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.RadioButton;
+import android.widget.TabHost;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MobilityActivity extends BaseActivity implements LoaderCallbacks<Cursor> {
-	
+
+public class MobilityActivity extends BaseActivity {
+
 	private static final String TAG = "MobilityActivity";
-	
-	private static final int RECENT_LOADER = 0;
-	private static final int ALL_LOADER = 1;
-	private static final int UPLOAD_LOADER = 2;
-	
-	private ListView mMobilityList;
-	private TextView mTotalCountText;
-	private TextView mUploadCountText;
-	private TextView mLastUploadText;
-	private Button mUploadButton;
-	private RadioButton mOffRadio;
-	private RadioButton mInterval1Radio;
-	private RadioButton mInterval5Radio;
-	
-	private SimpleCursorAdapter mAdapter;
-	
-	private SharedPreferencesHelper mPrefHelper;
-	private IMobility mMobility = null;
-	private boolean isBound = false;
 
-	private final String emptyValue = "-";
-
-	/**
-	 * Will be set to true when the {@link #UPLOAD_LOADER} loader
-	 * should make sure all points uploaded
-	 */
-	private boolean mVerifyNoPoints;
+	TabHost mTabHost;
+	ViewPager mViewPager;
+	TabsAdapter mTabsAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		boolean isMobilityInstalled = false;
-		
+
 		try {
 			ApplicationInfo info = getPackageManager().getApplicationInfo("edu.ucla.cens.mobility", 0 );
 			ApplicationInfo info2 = getPackageManager().getApplicationInfo("edu.ucla.cens.accelservice", 0 );
-			
+
 			if (info != null && info2 != null) {
 				isMobilityInstalled = true;
 			}
 		} catch( PackageManager.NameNotFoundException e ) {
 			isMobilityInstalled = false;
 		}
-		    
+
 		if (! isMobilityInstalled) {
 			TextView view = new TextView(this);
 			view.setText("Please make sure the Mobility and AccelService packages are installed.");
@@ -95,299 +48,28 @@ public class MobilityActivity extends BaseActivity implements LoaderCallbacks<Cu
 			view.setGravity(Gravity.CENTER);
 			setContentView(view);
 		} else {
-			setContentView(R.layout.mobility_layout);
-			
-			mMobilityList = (ListView) findViewById(R.id.mobility_list);
-			mTotalCountText = (TextView) findViewById(R.id.mobility_total);
-			mUploadCountText = (TextView) findViewById(R.id.mobility_count);
-			mLastUploadText = (TextView) findViewById(R.id.last_upload);
-			mUploadButton = (Button) findViewById(R.id.upload_button);
-			mOffRadio = (RadioButton) findViewById(R.id.radio_off);
-			mInterval1Radio = (RadioButton) findViewById(R.id.radio_1min);
-			mInterval5Radio = (RadioButton) findViewById(R.id.radio_5min);
-			
-			mPrefHelper = new SharedPreferencesHelper(this);
-			
-			TextView emptyView = (TextView) findViewById(R.id.empty);
-			mMobilityList.setEmptyView(emptyView);
-			
-//			mMobilityList.setEnabled(false);
-			
-			String [] from = new String[] {MobilityInterface.KEY_MODE, MobilityInterface.KEY_TIME};
-			int [] to = new int[] {R.id.text1, R.id.text2};
-			
-			mAdapter = new SimpleCursorAdapter(this, R.layout.mobility_list_item, null, from, to, 0);
-			
-			mAdapter.setViewBinder( new android.support.v4.widget.SimpleCursorAdapter.ViewBinder() {
-				
-				@Override
-				public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
-					switch (view.getId()) {
-					case R.id.text1:
-						((TextView)view).setText(cursor.getString(columnIndex));
-						return true;
-					
-					case R.id.text2:
-						long time = cursor.getLong(columnIndex);
-						((TextView)view).setText(DateFormat.format("h:mmaa", time));
-						return true;
-					}
-					return false;
-				}
-			});
-			
-			mMobilityList.setAdapter(mAdapter);
-			
-			mTotalCountText.setText(emptyValue);
-			mUploadCountText.setText(emptyValue);
-			long lastMobilityUploadTimestamp = mPrefHelper.getLastMobilityUploadTimestamp();
-			if (lastMobilityUploadTimestamp == 0) {
-				mLastUploadText.setText(emptyValue);
-			} else {
-				mLastUploadText.setText(DateFormat.format("yyyy-MM-dd kk:mm:ss", lastMobilityUploadTimestamp));
-			}
-			
-			getSupportLoaderManager().initLoader(RECENT_LOADER, null, this);
-			getSupportLoaderManager().initLoader(ALL_LOADER, null, this);
-			getSupportLoaderManager().initLoader(UPLOAD_LOADER, null, this);
-			
-			mOffRadio.setChecked(true);
-			
-			mUploadButton.setOnClickListener(mUploadListener);
-			
-			bindService(new Intent(IMobility.class.getName()), mConnection, Context.BIND_AUTO_CREATE);
-			isBound = true;
-		}
-	}
-	
-	@Override
-	protected void onStart() {
-		super.onStart();
-		registerReceiver(mMobilityUploadReceiver, new IntentFilter(UploadService.MOBILITY_UPLOAD_STARTED));
-		registerReceiver(mMobilityUploadReceiver, new IntentFilter(UploadService.MOBILITY_UPLOAD_FINISHED));
-	}
+			setContentView(R.layout.tab_layout);
+			setActionBarShadowVisibility(false);
 
-	@Override
-	protected void onStop() {
-		super.onStop();
-		unregisterReceiver(mMobilityUploadReceiver);
-	}
-	
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		
-		if (isBound) {
-			unbindService(mConnection);
-		}
-	}
-	
-	private final BroadcastReceiver mMobilityUploadReceiver = new BroadcastReceiver() {
-		
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			String action = intent.getAction();
-			
-			if (UploadService.MOBILITY_UPLOAD_STARTED.equals(action)) {
-				MobilityActivity.this.getActionBar().setProgressVisible(true);
-				MobilityActivity.this.mUploadButton.setEnabled(false);
-				MobilityActivity.this.mUploadButton.setText("Uploading...");
-			} else if (UploadService.MOBILITY_UPLOAD_FINISHED.equals(action)) {
-				MobilityActivity.this.getActionBar().setProgressVisible(false);
-				MobilityActivity.this.mUploadButton.setEnabled(true);
-				MobilityActivity.this.mUploadButton.setText("Upload Now");
-				Long lastMobilityUploadTimestamp = mPrefHelper.getLastMobilityUploadTimestamp();
-				if(lastMobilityUploadTimestamp != 0) {
-					mLastUploadText.setText(DateFormat.format("yyyy-MM-dd kk:mm:ss", lastMobilityUploadTimestamp));
-				}
-				mVerifyNoPoints = true;
-				getSupportLoaderManager().restartLoader(UPLOAD_LOADER, null, MobilityActivity.this);
-			}
-		}
-	};
-	
-	private final OnClickListener mUploadListener = new OnClickListener() {
-		
-		@Override
-		public void onClick(View v) {
-			Analytics.widget(v);
-			Intent intent = new Intent(MobilityActivity.this, UploadService.class);
-			intent.setData(Responses.CONTENT_URI);
-			intent.putExtra(UploadService.EXTRA_UPLOAD_MOBILITY, true);
-			WakefulIntentService.sendWakefulWork(MobilityActivity.this, intent);
-		}
-	};
-	
-	private final ServiceConnection mConnection = new ServiceConnection() {
-		
-		@Override
-		public void onServiceDisconnected(ComponentName name) {
-			
-			mMobility = null;
-		}
-		
-		@Override
-		public void onServiceConnected(ComponentName name, IBinder service) {
-			
-			mMobility = IMobility.Stub.asInterface(service);
-			
-			int interval = -1;
-			boolean isMobilityOn = false;
-			
-			try {
-				isMobilityOn = mMobility.isMobilityOn();
-				interval = mMobility.getMobilityInterval();
-			} catch (RemoteException e) {
-				Log.e(TAG, "Unable to read mobility state", e);
-			}
-			
-			if (! isMobilityOn) {
-				mOffRadio.setChecked(true);
-			} else if (interval == 60) {
-				mInterval1Radio.setChecked(true);
-			} else if (interval == 300) {
-				mInterval5Radio.setChecked(true);
-			}
-			
-			mOffRadio.setOnClickListener(mRadioListener);			
-			mInterval1Radio.setOnClickListener(mRadioListener);
-			mInterval5Radio.setOnClickListener(mRadioListener);
-		}
-		
-		OnClickListener mRadioListener = new OnClickListener() {
-			
-			@Override
-			public void onClick(final View v) {
-				Analytics.widget(v);
+			mTabHost = (TabHost) findViewById(android.R.id.tabhost);
+			mTabHost.setup();
 
-				// do the update in an asynctask so the UI doesn't block
-				(new AsyncTask<Void, Void, Void>() {
-					@Override
-					protected void onPreExecute() {
-						mOffRadio.setEnabled(false);
-						mInterval1Radio.setEnabled(false);
-						mInterval5Radio.setEnabled(false);
-					};
-					
-					@Override
-					protected Void doInBackground(Void... params) {
-						int newInterval = -1;
-						int oldInterval = -1;
-						
-						switch (v.getId()) {
-						case R.id.radio_off:
-							try {
-								mMobility.stopMobility();
-							} catch (RemoteException e) {
-								Log.e(TAG, "Unable to stop mobility", e);
-							}
-							return null;
-							
-						case R.id.radio_1min:
-							newInterval = 60;
-							break;
+			mViewPager = (ViewPager) findViewById(R.id.pager);
 
-						case R.id.radio_5min:
-							newInterval = 300;
-							break;
-						}
-						
-						try {
-							oldInterval = mMobility.getMobilityInterval();
-							if (newInterval != oldInterval) {
-								mMobility.changeMobilityRate(newInterval);
-							}
-							
-							if (! mMobility.isMobilityOn()) {
-								mMobility.startMobility();
-							}
-						} catch (RemoteException e) {
-							Log.e(TAG, "Unable to change mobility interval", e);
-						}
-						
-						return null;
-					};
-					
-					@Override
-					protected void onPostExecute(Void result) {
-						mOffRadio.setEnabled(true);
-						mInterval1Radio.setEnabled(true);
-						mInterval5Radio.setEnabled(true);
-					};
-				}).execute();
+			mTabsAdapter = new TabsAdapter(this, mTabHost, mViewPager);
+
+			mTabsAdapter.addTab("Analytics", RecentMobilityChartFragment.class, null);
+			mTabsAdapter.addTab("Control", MobilityControlFragment.class, null);
+
+			if (savedInstanceState != null) {
+				mTabHost.setCurrentTabByTag(savedInstanceState.getString("tab"));
 			}
-		};
-	};
-	
-	public interface MobilityQuery {
-		String[] PROJECTION = { 
-				MobilityInterface.KEY_ROWID, 
-				MobilityInterface.KEY_MODE, 
-				MobilityInterface.KEY_TIME
-		};
-	}
-
-	@Override
-	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-		long loginTimestamp = mPrefHelper.getLoginTimestamp();
-
-		switch (id) {
-		case RECENT_LOADER:
-			return new CursorLoader(this, MobilityInterface.CONTENT_URI, MobilityQuery.PROJECTION, MobilityInterface.KEY_TIME + " > strftime('%s','now','-20 minutes') || '500' AND " + MobilityInterface.KEY_TIME + " > ?", new String[] {String.valueOf(loginTimestamp)}, MobilityInterface.KEY_TIME + " DESC");
-
-		case ALL_LOADER:
-			return new CursorLoader(this, MobilityInterface.CONTENT_URI, MobilityQuery.PROJECTION, MobilityInterface.KEY_TIME + " > ?", new String[] {String.valueOf(loginTimestamp)}, null);
-			
-		case UPLOAD_LOADER:
-			long uploadAfterTimestamp = mPrefHelper.getLastMobilityUploadTimestamp();
-			if (uploadAfterTimestamp == 0) {
-				uploadAfterTimestamp = loginTimestamp;
-			}
-			return new CursorLoader(this, MobilityInterface.CONTENT_URI, MobilityQuery.PROJECTION, MobilityInterface.KEY_TIME + " > ?", new String[] {String.valueOf(uploadAfterTimestamp)}, null);
-			
-		default:
-			return null;
 		}
 	}
 
 	@Override
-	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-		
-		switch (loader.getId()) {
-		case RECENT_LOADER:
-			mAdapter.swapCursor(data);
-			break;
-
-		case ALL_LOADER:
-			mTotalCountText.setText(String.valueOf(data.getCount()));
-			break;
-			
-		case UPLOAD_LOADER:
-			// If mobility upload finishes, but there are still points there was an error
-			if(mVerifyNoPoints && data.getCount() != 0) {
-				mVerifyNoPoints = false;
-				Toast.makeText(this, R.string.mobility_upload_error_message, Toast.LENGTH_SHORT).show();
-			}
-			mUploadCountText.setText(String.valueOf(data.getCount()));
-			break;
-		}
-	}
-
-	@Override
-	public void onLoaderReset(Loader<Cursor> loader) {
-		
-		switch (loader.getId()) {
-		case RECENT_LOADER:
-			mAdapter.swapCursor(null);
-			break;
-
-		case ALL_LOADER:
-			mTotalCountText.setText(emptyValue);
-			break;
-			
-		case UPLOAD_LOADER:
-			mUploadCountText.setText(emptyValue);
-			break;
-		}
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putString("tab", mTabHost.getCurrentTabTag());
 	}
 }
---
+++ src/org/ohmage/fragments/RecentMobilityChartFragment.java
@@ -0,0 +1,167 @@
+package org.ohmage.fragments;
+
+import edu.ucla.cens.mobility.glue.MobilityInterface;
+
+import org.achartengine.GraphicalView;
+import org.ohmage.MobilityHelper;
+import org.ohmage.R;
+import org.ohmage.SharedPreferencesHelper;
+import org.ohmage.charts.Histogram;
+import org.ohmage.charts.HistogramBase.HistogramRenderer;
+import org.ohmage.loader.PromptFeedbackLoader.FeedbackItem;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.text.format.DateUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+
+public class RecentMobilityChartFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+	private static final int MAX_DATA_COLUMNS = 10;
+
+	private static final int LOAD_MOBILITY_DATA = 0;
+
+	private ViewGroup mContainer;
+	private TextView mTodayStill;
+	private TextView mTodayWalk;
+	private TextView mTodayRun;
+	private TextView mTodayDrive;
+
+	private SharedPreferencesHelper mSharedPreferences;
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+		mSharedPreferences = new SharedPreferencesHelper(getActivity());
+
+		getLoaderManager().initLoader(LOAD_MOBILITY_DATA, null, this);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.recent_mobility_activity_fragment_layout, container, false);
+
+		mContainer = (ViewGroup) view.findViewById(R.id.feedback_response_graph);
+		mTodayStill = (TextView) view.findViewById(R.id.recent_mobility_still);
+		mTodayWalk = (TextView) view.findViewById(R.id.recent_mobility_walk);
+		mTodayRun = (TextView) view.findViewById(R.id.recent_mobility_run);
+		mTodayDrive = (TextView) view.findViewById(R.id.recent_mobility_drive);
+
+		return view;
+	}
+
+	public interface MobilityQuery {
+		String[] PROJECTION = {
+				MobilityInterface.KEY_DAY,
+				MobilityInterface.KEY_MODE,
+				MobilityInterface.KEY_DURATION
+		};
+	}
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		return new CursorLoader(getActivity(), MobilityInterface.AGGREGATES_URI,
+				MobilityQuery.PROJECTION, MobilityInterface.KEY_DAY
+				+ " > date('now', 'localtime', '-10 days') AND "
+				+ MobilityInterface.KEY_USERNAME + "=?", new String[] {
+			MobilityHelper.getMobilityUsername(mSharedPreferences.getUsername())
+		}, MobilityInterface.KEY_DAY + " DESC");
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		// Nothing to do?
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+		if(data == null)
+			return;
+
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		List<FeedbackItem> values = new LinkedList<FeedbackItem>();
+
+		// Move to the beginning of the data
+		data.moveToPosition(-1);
+
+		for (int i = 0; i < MAX_DATA_COLUMNS; i++) {
+			FeedbackItem point = new FeedbackItem();
+			point.time = System.currentTimeMillis() - DateUtils.DAY_IN_MILLIS * i;
+			point.value = 0.0;
+			// Count up the data for each day
+			try {
+				while(data.moveToNext()
+						&& DateUtils.isToday(formatter.parse(data.getString(0)).getTime() + DateUtils.DAY_IN_MILLIS * i)) {
+
+					String mode = data.getString(1);
+					if(MobilityInterface.WALK.equals(mode) || MobilityInterface.RUN.equals(mode) || MobilityInterface.BIKE.equals(mode))
+						point.value+= data.getDouble(2);
+
+					// If this came from today we should set the correct textview
+					if (DateUtils.isToday(formatter.parse(data.getString(0)).getTime())) {
+						if (MobilityInterface.STILL.equals(mode))
+							mTodayStill.setText(formatTimeAmount(data.getLong(2)));
+						else if (MobilityInterface.WALK.equals(mode))
+							mTodayWalk.setText(formatTimeAmount(data.getLong(2)));
+						else if (MobilityInterface.RUN.equals(mode))
+							mTodayRun.setText(formatTimeAmount(data.getLong(2)));
+						else if (MobilityInterface.DRIVE.equals(mode))
+							mTodayDrive.setText(formatTimeAmount(data.getLong(2)));
+					}
+				}
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			// Change the value from ms to hours
+			point.value /= DateUtils.HOUR_IN_MILLIS;
+			values.add(point);
+			data.moveToPrevious();
+		}
+
+		HistogramRenderer renderer = new HistogramRenderer(getActivity());
+		renderer.setMargins(new int[] {
+				30, 45, 15, 15
+		});
+		renderer.setYLabels(5);
+		renderer.setYAxisMin(0);
+		renderer.setYTitle("# of Hours");
+		renderer.setInScroll(true);
+
+		mContainer.removeAllViews();
+		mContainer.addView(new GraphicalView(getActivity(), new Histogram(getActivity(), renderer, values, MAX_DATA_COLUMNS)));
+	}
+
+	/**
+	 * Formats the time amount if its less than one hour it will return the
+	 * minutes. If it is greater than an hour it will return the number of
+	 * hours with one decimal point.
+	 * 
+	 * @param time
+	 * @return
+	 */
+	private CharSequence formatTimeAmount(long time) {
+		if(time / DateUtils.HOUR_IN_MILLIS < 1) {
+			return (time / DateUtils.MINUTE_IN_MILLIS) + " min";
+		} else {
+			NumberFormat numberFormatter = NumberFormat.getNumberInstance(Locale.US);
+			numberFormatter.setMaximumFractionDigits(1);
+			return numberFormatter.format(time / new Double(DateUtils.HOUR_IN_MILLIS)) + " h";
+		}
+	}
+}
