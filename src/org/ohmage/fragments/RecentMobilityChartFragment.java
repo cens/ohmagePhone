dd698efc31470983d21ecbbd3f227017be2524f9
Cameron Ketcham <cketcham@gmail.com>
2012-04-30 18:03:19 -0700

    Added baseline and last week mobility aggregate times

--- src/org/ohmage/fragments/RecentMobilityChartFragment.java
+++ src/org/ohmage/fragments/RecentMobilityChartFragment.java
@@ -6,8 +6,10 @@ import org.achartengine.GraphicalView;
 import org.ohmage.MobilityHelper;
 import org.ohmage.R;
 import org.ohmage.SharedPreferencesHelper;
+import org.ohmage.UserPreferencesHelper;
 import org.ohmage.charts.Histogram;
 import org.ohmage.charts.HistogramBase.HistogramRenderer;
+import org.ohmage.loader.MobilityAggregateLoader;
 import org.ohmage.loader.PromptFeedbackLoader.FeedbackItem;
 
 import android.database.Cursor;
@@ -25,6 +27,7 @@ import android.widget.TextView;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
@@ -35,7 +38,9 @@ public class RecentMobilityChartFragment extends Fragment implements LoaderManag
 
 	private static final int LOAD_MOBILITY_DATA = 0;
 
-	private static final int DOWNLOAD_MOBILITY_DATA = 1;
+	private static final int LOAD_MOBILITY_BASELINE_AGGREGATE = 1;
+
+	private static final int LOAD_MOBILITY_LASTWEEK_AGGREGATE = 2;
 
 	private ViewGroup mContainer;
 	private TextView mTodayStill;
@@ -45,6 +50,10 @@ public class RecentMobilityChartFragment extends Fragment implements LoaderManag
 
 	private SharedPreferencesHelper mSharedPreferences;
 
+	private TextView mAggregateBaseline;
+
+	private TextView mAggregateLastweek;
+
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
@@ -52,6 +61,8 @@ public class RecentMobilityChartFragment extends Fragment implements LoaderManag
 		mSharedPreferences = new SharedPreferencesHelper(getActivity());
 
 		getLoaderManager().initLoader(LOAD_MOBILITY_DATA, null, this);
+		getLoaderManager().initLoader(LOAD_MOBILITY_BASELINE_AGGREGATE, null, this);
+		getLoaderManager().initLoader(LOAD_MOBILITY_LASTWEEK_AGGREGATE, null, this);
 	}
 
 	@Override
@@ -63,6 +74,9 @@ public class RecentMobilityChartFragment extends Fragment implements LoaderManag
 		mTodayWalk = (TextView) view.findViewById(R.id.recent_mobility_walk);
 		mTodayRun = (TextView) view.findViewById(R.id.recent_mobility_run);
 		mTodayDrive = (TextView) view.findViewById(R.id.recent_mobility_drive);
+
+		mAggregateBaseline = (TextView) view.findViewById(R.id.aggregate_baseline_count);
+		mAggregateLastweek = (TextView) view.findViewById(R.id.aggregate_lastweek_count);
 
 		return view;
 	}
@@ -85,13 +99,32 @@ public class RecentMobilityChartFragment extends Fragment implements LoaderManag
 						+ MobilityInterface.KEY_USERNAME + "=?", new String[] {
 					MobilityHelper.getMobilityUsername(mSharedPreferences.getUsername())
 				}, MobilityInterface.KEY_DAY + " DESC");
+			case LOAD_MOBILITY_BASELINE_AGGREGATE:
+				return new MobilityAggregateLoader(getActivity(),
+						UserPreferencesHelper.getBaseLineEndTime(getActivity()),
+						mSharedPreferences.getUsername());
+			case LOAD_MOBILITY_LASTWEEK_AGGREGATE:
+				Calendar cal = Calendar.getInstance();
+				cal.add(Calendar.DATE, -7);
+				return new MobilityAggregateLoader(getActivity(),
+						cal.getTimeInMillis(),
+						mSharedPreferences.getUsername());
 		}
 		return null;
 	}
 
 	@Override
 	public void onLoaderReset(Loader<Cursor> loader) {
-		// Nothing to do?
+		if(LOAD_MOBILITY_DATA == loader.getId()) {
+			mTodayStill.setText(null);
+			mTodayWalk.setText(null);
+			mTodayRun.setText(null);
+			mTodayDrive.setText(null);
+		} if(LOAD_MOBILITY_BASELINE_AGGREGATE == loader.getId()) {
+			mAggregateBaseline.setText(R.string.empty_value);
+		} else if(LOAD_MOBILITY_LASTWEEK_AGGREGATE == loader.getId()) {
+			mAggregateLastweek.setText(R.string.empty_value);
+		}
 	}
 
 	@Override
@@ -99,58 +132,68 @@ public class RecentMobilityChartFragment extends Fragment implements LoaderManag
 		if(data == null)
 			return;
 
-		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-		List<FeedbackItem> values = new LinkedList<FeedbackItem>();
-
 		// Move to the beginning of the data
 		data.moveToPosition(-1);
 
-		for (int i = 0; i < MAX_DATA_COLUMNS; i++) {
-			FeedbackItem point = new FeedbackItem();
-			point.time = System.currentTimeMillis() - DateUtils.DAY_IN_MILLIS * i;
-			point.value = 0.0;
-			// Count up the data for each day
-			try {
-				while(data.moveToNext()
-						&& DateUtils.isToday(formatter.parse(data.getString(0)).getTime() + DateUtils.DAY_IN_MILLIS * i)) {
+		if(LOAD_MOBILITY_DATA == loader.getId()) {
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+			List<FeedbackItem> values = new LinkedList<FeedbackItem>();
 
-					String mode = data.getString(1);
-					if(MobilityInterface.WALK.equals(mode) || MobilityInterface.RUN.equals(mode) || MobilityInterface.BIKE.equals(mode))
-						point.value+= data.getDouble(2);
+			for (int i = 0; i < MAX_DATA_COLUMNS; i++) {
+				FeedbackItem point = new FeedbackItem();
+				point.time = System.currentTimeMillis() - DateUtils.DAY_IN_MILLIS * i;
+				point.value = 0.0;
+				// Count up the data for each day
+				try {
+					while(data.moveToNext()
+							&& DateUtils.isToday(formatter.parse(data.getString(0)).getTime() + DateUtils.DAY_IN_MILLIS * i)) {
 
-					// If this came from today we should set the correct textview
-					if (DateUtils.isToday(formatter.parse(data.getString(0)).getTime())) {
-						if (MobilityInterface.STILL.equals(mode))
-							mTodayStill.setText(formatTimeAmount(data.getLong(2)));
-						else if (MobilityInterface.WALK.equals(mode))
-							mTodayWalk.setText(formatTimeAmount(data.getLong(2)));
-						else if (MobilityInterface.RUN.equals(mode))
-							mTodayRun.setText(formatTimeAmount(data.getLong(2)));
-						else if (MobilityInterface.DRIVE.equals(mode))
-							mTodayDrive.setText(formatTimeAmount(data.getLong(2)));
+						String mode = data.getString(1);
+						if(MobilityInterface.WALK.equals(mode) || MobilityInterface.RUN.equals(mode) || MobilityInterface.BIKE.equals(mode))
+							point.value+= data.getDouble(2);
+
+						// If this came from today we should set the correct textview
+						if (DateUtils.isToday(formatter.parse(data.getString(0)).getTime())) {
+							if (MobilityInterface.STILL.equals(mode))
+								mTodayStill.setText(formatTimeAmount(data.getLong(2)));
+							else if (MobilityInterface.WALK.equals(mode))
+								mTodayWalk.setText(formatTimeAmount(data.getLong(2)));
+							else if (MobilityInterface.RUN.equals(mode))
+								mTodayRun.setText(formatTimeAmount(data.getLong(2)));
+							else if (MobilityInterface.DRIVE.equals(mode))
+								mTodayDrive.setText(formatTimeAmount(data.getLong(2)));
+						}
 					}
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				// Change the value from ms to hours
+				point.value /= DateUtils.HOUR_IN_MILLIS;
+				values.add(point);
+				data.moveToPrevious();
 			}
-			// Change the value from ms to hours
-			point.value /= DateUtils.HOUR_IN_MILLIS;
-			values.add(point);
-			data.moveToPrevious();
+
+			HistogramRenderer renderer = new HistogramRenderer(getActivity());
+			renderer.setMargins(new int[] {
+					30, 45, 15, 15
+			});
+			renderer.setYLabels(5);
+			renderer.setYAxisMin(0);
+			renderer.setYTitle("# of Hours");
+			renderer.setInScroll(true);
+
+			mContainer.removeAllViews();
+			mContainer.addView(new GraphicalView(getActivity(), new Histogram(getActivity(), renderer, values, MAX_DATA_COLUMNS)));
+		} else if(LOAD_MOBILITY_BASELINE_AGGREGATE == loader.getId()) {
+			long time = ((MobilityAggregateLoader) loader).getTimeActive();
+			if(time != 0)
+				mAggregateBaseline.setText(formatTimeAmount(time));
+		} else if(LOAD_MOBILITY_LASTWEEK_AGGREGATE == loader.getId()) {
+			long time = ((MobilityAggregateLoader) loader).getTimeActive();
+			if(time != 0)
+				mAggregateLastweek.setText(formatTimeAmount(time));
 		}
-
-		HistogramRenderer renderer = new HistogramRenderer(getActivity());
-		renderer.setMargins(new int[] {
-				30, 45, 15, 15
-		});
-		renderer.setYLabels(5);
-		renderer.setYAxisMin(0);
-		renderer.setYTitle("# of Hours");
-		renderer.setInScroll(true);
-
-		mContainer.removeAllViews();
-		mContainer.addView(new GraphicalView(getActivity(), new Histogram(getActivity(), renderer, values, MAX_DATA_COLUMNS)));
 	}
 
 	/**
