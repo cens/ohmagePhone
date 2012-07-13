7f9e1f68d3fddf8ac2b4910dde1b1c05efe4b0fa
Cameron Ketcham <cketcham@gmail.com>
2012-07-13 10:04:50 -0700

    added in memory buffer for writing points to storage in ohmage

--- src/org/ohmage/probemanager/ProbeManager.java
+++ src/org/ohmage/probemanager/ProbeManager.java
@@ -4,12 +4,41 @@ package org.ohmage.probemanager;
 import android.app.Service;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.os.RemoteException;
 
 import org.ohmage.probemanager.DbContract.Probes;
 
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+
 public class ProbeManager extends Service {
+
+    private static final boolean BUFFER_POINTS = true;
+    private static final int MAX_BUFFER = 600;
+    private static final long FLUSH_DELAY = 300;
+
+    ArrayList<ContentValues> points = new ArrayList<ContentValues>();
+
+    static class PointFlushHandler extends Handler {
+        private final WeakReference<ProbeManager> mService;
+
+        PointFlushHandler(ProbeManager service) {
+            mService = new WeakReference<ProbeManager>(service);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            ProbeManager service = mService.get();
+            if (service != null) {
+                service.flushPoints();
+            }
+        }
+    }
+
+    PointFlushHandler mHandler = new PointFlushHandler(this);
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -27,8 +56,46 @@ public class ProbeManager extends Service {
                 values.put(Probes.UPLOAD_PRIORITY, uploadPriority);
                 values.put(Probes.PROBE_METADATA, metadata);
                 values.put(Probes.PROBE_DATA, data);
-                return getContentResolver().insert(Probes.CONTENT_URI, values) != null;
+
+                if (BUFFER_POINTS) {
+                    synchronized (points) {
+                        points.add(values);
+                        if (points.size() > MAX_BUFFER)
+                            flushPoints();
+                        else
+                            queueFlush();
+                    }
+                    return true;
+                } else {
+                    return getContentResolver().insert(Probes.CONTENT_URI, values) != null;
+                }
             }
         };
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        flushPoints();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        flushPoints();
+    }
+
+    private void flushPoints() {
+        ContentValues[] toFlush;
+        synchronized (points) {
+            toFlush = points.toArray(new ContentValues[] {});
+            points.clear();
+        }
+        getContentResolver().bulkInsert(Probes.CONTENT_URI, toFlush);
+    }
+
+    private void queueFlush() {
+        mHandler.removeMessages(0);
+        mHandler.sendEmptyMessageDelayed(0, FLUSH_DELAY);
     }
 }
